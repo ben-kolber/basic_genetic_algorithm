37fef9426d14f9c4dbf6ff2ccab41a9b30c8b544
booleanlogic21 <46203448+booleanlogic21@users.noreply.github.com>
2019-11-28 17:56:30 +0800

    Update dna.py

--- dna.py
+++ dna.py
@@ -1,13 +1,12 @@
-
 import random
 
 class dna:
-
     def __init__(self, target):
         self.length = len(target)
         self.genes = self.create_genes(self.length)
         self.score = 0
 
+    # list of random numbers to represent a gene
     def create_genes(self, length):
         list = []
         for i in range(length):
@@ -19,6 +18,7 @@ class dna:
         print("SCORE: {}".format(self.score))
         print("-"*20)
 
+    # calculate fitness of DNA based on end goal
     def fitness(self, target):
         for i in range(self.length):
             if (self.genes[i] == target[i]):
