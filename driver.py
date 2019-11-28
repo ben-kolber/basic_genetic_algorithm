4cbbc568dec8959aaae026abcb78bef691f4faa3
booleanlogic21 <46203448+booleanlogic21@users.noreply.github.com>
2019-11-28 17:57:53 +0800

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,26 +1,22 @@
 from dna import dna
 from population import population
 
-import time
-
+# size of a population
 pop_size = 200
+# target string population will end up generating
+# starting from a random string
 target = "Hello"
 
-
 population = population(pop_size, target)
-
 population.update_fitness()
 population.print_population()
 
-time.sleep(1)
-
-
+# range is the number of generations that will be created 
 for i in range(30):
     print("-" * 30)
     print("GENERATION:: {}".format(i))
     print("-" * 30)
     time.sleep(0.1)
-
     population.run_mating_pool()
     population.update_fitness()
     population.print_population()
